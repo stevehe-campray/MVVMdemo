f22098726c5142f3f1859c109e470aa64d1c6645
stevehe-campray <602258720@qq.com>
2017-03-01 14:52:48 +0800

    init  MVVMDemo

---
+++ MVVMdemo/MVVMdemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MVVMdemo
+//
+//  Created by hejingjin on 17/3/1.
+//  Copyright © 2017年 teamsun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
